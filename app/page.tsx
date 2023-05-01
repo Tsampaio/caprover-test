0364da3f3060d7d59f57d862655611d881bffa29
Telmo Sampaio <telmoasampaio@gmail.com>
2023-05-01 15:10:01 +0100

    adding white text

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       <div className="container mx-auto mt-5">
         <h1 className='text-white text-5xl text-center'>Hello world</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui vel dignissimos officiis fugiat maiores, ea tempore, doloremque dolorum, aliquam voluptatem ut ratione eos nam illo fugit aspernatur quod porro quidem.</p>
+        <p className='text-white'>One more text</p>
       </div>
     </main >
   )
