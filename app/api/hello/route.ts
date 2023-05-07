e4fa023d7e7484602e86e059642ee26f13e55cd3
Telmo Sampaio <telmoasampaio@gmail.com>
2023-05-07 17:06:49 +0100

    trying to fix prisma

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient()
 export async function GET(request: Request) {
   console.log('hello')
 
-  const users = await prisma.user.findMany()
+  const users = await prisma.user.findMany() || []
   console.log(users)
 
   return NextResponse.json({ users: users });
