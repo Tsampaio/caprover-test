41bf5108387faef180eafc7e8ebd85ad2cf60f24
Telmo Sampaio <telmoasampaio@gmail.com>
2023-05-07 16:58:06 +0100

    fixing prisma

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server';
 
+const prisma = new PrismaClient()
+
 export async function GET(request: Request) {
   console.log('hello')
-  const prisma = new PrismaClient()
-
 
   const users = await prisma.user.findMany()
   console.log(users)
